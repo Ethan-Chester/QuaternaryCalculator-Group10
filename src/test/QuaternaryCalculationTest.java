098026956eebe4f75575d0d5218fc18f6473532c
Jadi Miller <jadilmiller@gmail.com>
2024-09-03 14:46:32 -0400

    Create tests for power function

--- src/test/QuaternaryCalculationTest.java
+++ src/test/QuaternaryCalculationTest.java
@@ -7,17 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class QuaternaryCalculationTest {
 
     @Test
-    void tentofour() {
-    }
-
-    @Test
-    void squareRoot() {
+    void squareRootShouldReturnCorrectAnswer() {
         assertEquals(2, QuaternaryCalculation.squareRoot(4));
     }
 
     @Test
-    void power() {
+    void powerShouldReturnCorrectAnswer() {
         assertEquals(4, QuaternaryCalculation.power(2, 2));
+        assertEquals(1, QuaternaryCalculation.power(1,1));
+    }
+
+    @Test
+    void powerShouldReturnOneIfExponentIsZero() {
+        assertEquals(1, QuaternaryCalculation.power(3,0));
+        assertEquals(1, QuaternaryCalculation.power(2,0));
+        assertEquals(1, QuaternaryCalculation.power(1,0));
+        assertEquals(1, QuaternaryCalculation.power(0,0));
     }
 }
 
