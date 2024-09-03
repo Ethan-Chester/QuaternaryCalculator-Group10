3e42fcb831a8ecc59dd64c1577f5623dd8a2d7f0
Ethan Chester <43710677+ethan-chester@users.noreply.github.com>
2024-09-03 01:00:48 -0400

    base4 to base10 conversion in QuaternaryCalculation Class

--- src/QuaternaryCalculation.java
+++ src/QuaternaryCalculation.java
@@ -1,38 +1,46 @@
 public class QuaternaryCalculation {
     public static void main(String[] args){
-//        + ADD, - SUBTRACT, * MULTIPLY, / DIVIDE, ^ SQ, & SQRT
+        // + ADD, - SUBTRACT, * MULTIPLY, / DIVIDE, ^ SQ, & SQRT
 
-        String[] numbers = args[0].split("%");
         String operator = args[1];
 
-        String number1 = numbers[0];
-        String number2 = numbers[1];
+        String number1 = args[0];
+        String number2 = args[2];
+
 
         int base10Int1 = 0;
         int base10Int2 = 0;
 
-        String base10Result = "";
-        String base4Result = "";
+        for (int i = 0; i < number1.length(); i++){
 
-        for (int i = number1.length() - 1; i > 0; i--){
+            //        if (Integer.parseInt(i) > 3{
+            //            //Throw an out of bounds error here
+            //        }
+
             char digitChar = number1.charAt(i);
             int qNumber = Character.getNumericValue(digitChar);
-
-            base10Int1 += qNumber*power(4, i);
+            base10Int1 += qNumber*power(4, number1.length() - 1 - i);
         }
-        if (!number2.isEmpty()) {
-            for (int i = number2.length() - 1; i > 0; i--) {
+        if (!number2.equals("$")) {
+            for (int i = 0; i < number2.length(); i++) {
+
+                //        if (Integer.parseInt(i) > 3{
+                //            //Throw an out of bounds error here
+                //        }
+
                 char digitChar = number2.charAt(i);
                 int qNumber = Character.getNumericValue(digitChar);
 
-                base10Int2 += qNumber * power(4, i);
+                base10Int2 += qNumber * power(4, number2.length() - 1 - i);
             }
         }
+
 
         try{
             switch (operator) {
                 case "+":
                     System.out.println(base10Int1+base10Int2);
+                    //return base10 and base4 in each switch case using base4 conversion function
                     break;
                 case "-":
                     System.out.println(base10Int1-base10Int2);
@@ -49,13 +57,20 @@ public class QuaternaryCalculation {
                     break;
                 case "&":
 //                  Square Root
+                    System.out.println(squareRoot(base10Int1));
                     break;
                 default:
-                    return;
+                    // Return Type Error (Enter a valid operator)
             }
         }catch(Exception e){
             System.out.println("Error: " + e.getMessage());
         }
+    }
+
+    //add Base10 to Base4 Conversion Function
+
+    public static int squareRoot(int base){
+        return (int) Math.round(Math.sqrt(base));
     }
 
     public static int power(int base, int exponent) {
